a04686758ba88bc182fb0bf1ed01e143c01bda9a
James Eagle <jimeagle@gmail.com>
2022-02-15 21:49:11 +0000

    Create transport helper

---
+++ transport.go
@@ -0,0 +1,47 @@
+package flaresolverr
+
+import (
+	"io"
+	"net/http"
+	"strings"
+)
+
+func NewTransport(client Client) *roundTripper {
+	return &roundTripper{
+		client: client,
+	}
+}
+
+type roundTripper struct {
+	client Client
+}
+
+func (r roundTripper) RoundTrip(request *http.Request) (resp *http.Response, err error) {
+
+	var rr *RequestResponse
+
+	if request.Method == http.MethodGet {
+		ops := RequestGetOps{}
+		ops.URL = request.URL.String()
+		rr, err = r.client.RequestGet(ops)
+	} else {
+		ops := RequestPostOps{}
+		ops.URL = request.URL.String()
+		rr, err = r.client.RequestPost(ops)
+	}
+
+	resp = &http.Response{}
+	resp.StatusCode = rr.Solution.Status
+	resp.Status = http.StatusText(rr.Solution.Status)
+	resp.Body = io.NopCloser(strings.NewReader(rr.Solution.Response))
+
+	for k, v := range rr.Solution.Headers {
+		resp.Header.Set(k, v)
+	}
+
+	resp.ContentLength = -1
+	resp.Uncompressed = true
+	resp.Request = request
+
+	return resp, nil
+}
