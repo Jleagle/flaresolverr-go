7e0f8f9e80da08d467c97d675a9dab2df168a9b8
Jleagle <jimeagle@gmail.com>
2022-02-16 12:56:21 +0000

    Validate options

--- options.go
+++ options.go
@@ -12,31 +12,41 @@ type Option func(*Client)
 
 func WithProtocol(proto string) Option {
 	return func(c *Client) {
-		c.proto = proto
+		if proto != "" {
+			c.proto = proto
+		}
 	}
 }
 
 func WithPort(port int) Option {
 	return func(c *Client) {
-		c.port = port
+		if port > 0 {
+			c.port = port
+		}
 	}
 }
 
 func WithHostName(hostname string) Option {
 	return func(c *Client) {
-		c.hostname = hostname
+		if hostname != "" {
+			c.hostname = hostname
+		}
 	}
 }
 
 func WithClient(httpclient *http.Client) Option {
 	return func(c *Client) {
-		c.httpClient = httpclient
+		if httpclient != nil {
+			c.httpClient = httpclient
+		}
 	}
 }
 
 func WithRate(duration time.Duration, ctx context.Context) Option {
 	return func(c *Client) {
-		c.limiter = rate.NewLimiter(rate.Every(duration), 1)
-		c.limiterContext = ctx
+		if duration > 0 {
+			c.limiter = rate.NewLimiter(rate.Every(duration), 1)
+			c.limiterContext = ctx
+		}
 	}
 }
